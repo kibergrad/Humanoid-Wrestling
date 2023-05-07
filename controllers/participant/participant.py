a5ba7ab3f7c6f467dc2b62e207d683681cc53e11
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-05-07 10:55:31 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -24,16 +24,35 @@ sys.path.append('..')
 from utils.motion_library import MotionLibrary
 
 
-class Wrestler (Robot):
-    def run(self):
-        # to load all the motions from the motions folder, we use the MotionLibrary class:
-        motion_library = MotionLibrary()
+#class Wrestler (Robot):
+#    def run(self):
+#        # to load all the motions from the motions folder, we use the MotionLibrary class:
+#        motion_library = MotionLibrary()
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        time_step = int(self.getBasicTimeStep())
-        while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            motion_library.play('Backwards')
+#        time_step = int(self.getBasicTimeStep())
+#        while self.step(time_step) != -1:  # mandatory function to make the simulation run
+#            motion_library.play('Backwards')
 
 
 # create the Robot instance and run main loop
-wrestler = Wrestler()
+#wrestler = Wrestler()
+#wrestler.run()
+
+from controller import Robot, Motion
+
+
+class Alice (Robot):
+    def run(self):
+        # motion files are text files containing pre-recorded positions of the robot's joints
+        handWave = Motion('../motions/HandWave.motion')
+        handWave.setLoop(True)
+        handWave.play()
+        # retrieves the simulation time step (ms) from the world file
+        time_step = int(self.getBasicTimeStep())
+        while self.step(time_step) != -1:  # Mandatory function to make the simulation run
+            pass
+
+
+# create the Robot instance and run main loop
+wrestler = Alice()
 wrestler.run()
