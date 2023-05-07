bc9477e1a86cd8e1b85d53bc6b60f2a978304149
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-05-07 15:31:43 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,58 +1,37 @@
-# Copyright 1996-2023 Cyberbotics Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Minimalist controller example for the Robot Wrestling Tournament.
-   Demonstrates how to play a simple motion file."""
-
 from controller import Robot
 import sys
-
-# We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
-# If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
 from utils.motion_library import MotionLibrary
 
 
-#class Wrestler (Robot):
-#    def run(self):
-#        # to load all the motions from the motions folder, we use the MotionLibrary class:
-#        motion_library = MotionLibrary()
-        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-#        time_step = int(self.getBasicTimeStep())
-#        while self.step(time_step) != -1:  # mandatory function to make the simulation run
-#            motion_library.play('Backwards')
+class Charlie (Robot):
+    def __init__(self):
+        Robot.__init__(self)
+        self.time_step = int(self.getBasicTimeStep())
 
+        # there are 7 controllable LEDs on the NAO robot, but we will use only the ones in the eyes
+        self.leds = {
+            'right': self.getDevice('Face/Led/Right'),
+            'left':  self.getDevice('Face/Led/Left')
+        }
 
-# create the Robot instance and run main loop
-#wrestler = Wrestler()
-#wrestler.run()
+        self.library = MotionLibrary()
+        # adding a custom motion to the library
+        self.library.add('Shove', './Shove.motion', loop=True)
 
-from controller import Robot, Motion
-
-
-class Alice (Robot):
     def run(self):
-        # motion files are text files containing pre-recorded positions of the robot's joints
-        handWave = Motion('../motions/HandWave.motion')
-        handWave.setLoop(True)
-        handWave.play()
-        # retrieves the simulation time step (ms) from the world file
-        time_step = int(self.getBasicTimeStep())
-        while self.step(time_step) != -1:  # Mandatory function to make the simulation run
-            pass
+        self.library.play('Stand')
+
+        self.leds['right'].set(0xff0000)  # set the eyes to red
+        self.leds['left'].set(0xff0000)
+
+        while self.step(self.time_step) != -1:
+            # When the robot is done standing for stabilization, it moves forwards
+            if self.library.get('Stand').isOver():
+                self.library.play('ForwardLoop')  # walk forward
+                self.library.play('Custom')        # play the shove motion
 
 
 # create the Robot instance and run main loop
-wrestler = Alice()
+wrestler = Charlie()
 wrestler.run()
