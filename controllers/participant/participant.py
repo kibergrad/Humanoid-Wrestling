ebe48ddbfdcd85e37a38fee2b4b5dfb93a4c285d
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-05-11 13:17:02 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -25,7 +25,7 @@ class Sultaan (Robot):
         self.gait_manager = GaitManager(self, self.time_step)
         self.heading_angle = 3.14 / 2
         self.counter = 0
-        self.library.add('Cust2', './Cust2.motion', loop = True)
+        self.library.add('HandStill', './HandStill.motion', loop = True)
         self.leds = {
             'rightf': self.getDevice('Face/Led/Right'), 
             'leftf': self.getDevice('Face/Led/Left'), 
@@ -69,7 +69,7 @@ class Sultaan (Robot):
                 self.counter = 0
         self.counter += 1
         self.gait_manager.command_to_motors(desired_radius=desired_radius, heading_angle=self.heading_angle)
-        self.library.play('Cust2')
+        self.library.play('HandStill')
 
     def _get_normalized_opponent_x(self):
         """Locate the opponent in the image and return its horizontal position in the range [-1, 1]."""
