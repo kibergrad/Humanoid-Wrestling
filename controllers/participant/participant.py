8b99e2eda9957529d6f50fc843979a173648f4dc
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-05-12 19:46:04 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -13,7 +13,7 @@ from utils.camera import Camera
 class Dark_Lord(Robot):
     SMALLEST_TURNING_RADIUS = 0.05
     SAFE_ZONE = 0.75
-    TIME_BEFORE_DIRECTION_CHANGE = 20  # 8000 ms / 40 ms/
+    TIME_BEFORE_DIRECTION_CHANGE = 40  # 8000 ms / 40 ms/
 
     def __init__(self):
         Robot.__init__(self)
@@ -25,7 +25,7 @@ class Dark_Lord(Robot):
         self.gait_manager = GaitManager(self, self.time_step)
         self.heading_angle = 3.14 / 2
         self.counter = 0
-        self.library.add('First', './Fast.motion', loop = True)
+        self.library.add('First', './Shove.motion', loop = True)
         self.leds = {
             'rightf': self.getDevice('Face/Led/Right'), 
             'leftf': self.getDevice('Face/Led/Left'), 
