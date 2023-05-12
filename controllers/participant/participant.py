fe16cffbf17cf842c5bc5d07103ef3c09c092692
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-05-12 19:49:43 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,3 +1,4 @@
+
 from controller import Robot
 import sys
 sys.path.append('..')
@@ -8,15 +9,17 @@ from utils.image_processing import ImageProcessing as IP
 from utils.fall_detection import FallDetection
 from utils.gait_manager import GaitManager
 from utils.camera import Camera
+from utils.finite_state_machine import FiniteStateMachine
 
 
-class Dark_Lord(Robot):
-    SMALLEST_TURNING_RADIUS = 0.05
+class Sultaan (Robot):
+    SMALLEST_TURNING_RADIUS = 0.06
     SAFE_ZONE = 0.75
     TIME_BEFORE_DIRECTION_CHANGE = 40  # 8000 ms / 40 ms/
 
     def __init__(self):
         Robot.__init__(self)
+        self.fall = False
         self.time_step = int(self.getBasicTimeStep())
         self.library = MotionLibrary()
 
@@ -25,7 +28,7 @@ class Dark_Lord(Robot):
         self.gait_manager = GaitManager(self, self.time_step)
         self.heading_angle = 3.14 / 2
         self.counter = 0
-        self.library.add('First', './Shove.motion', loop = True)
+        self.library.add('Anglehandupdown', './Shove.motion', loop = True)
         self.leds = {
             'rightf': self.getDevice('Face/Led/Right'), 
             'leftf': self.getDevice('Face/Led/Left'), 
@@ -33,43 +36,61 @@ class Dark_Lord(Robot):
             'lefte': self.getDevice('Ears/Led/Left'), 
             'chest': self.getDevice('ChestBoard/Led'), 
         }
+
+
+        # for locking motor
+        joints = ['HipYawPitch', 'HipRoll', 'HipPitch', 'KneePitch', 'AnklePitch', 'AnkleRoll']
+        self.L_leg_motors = []
+        for joint in joints:
+            motor = self.getDevice(f'L{joint}')
+            position_sensor = motor.getPositionSensor()
+            position_sensor.enable(1)
+            self.L_leg_motors.append(motor)
+
+        self.R_leg_motors = []
+        for joint in joints:
+            motor = self.getDevice(f'R{joint}')
+            position_sensor = motor.getPositionSensor()
+            position_sensor.enable(1)
+            self.R_leg_motors.append(motor)
+
     def run(self):
         while self.step(self.time_step) != -1:
             # We need to update the internal theta value of the gait manager at every step:
             t = self.getTime()
-            # self.LeftHand.setPosition(-0.3)
-            # self.RightHand.setPosition(0.3)
             self.leds['rightf'].set(0xff0000)
             self.leds['leftf'].set(0xff0000)
             self.leds['righte'].set(0xff0000)
             self.leds['lefte'].set(0xff0000)
             self.leds['chest'].set(0xff0000)
-
             self.gait_manager.update_theta()
-            if 0.3 < t < 3:
+            if(self.fall_detector.detect_fall()): 
+                self.fall = True
+            if 0.3 < t < 2:
                 self.start_sequence()
-            elif t > 3:
+            elif t > 2:
                 self.fall_detector.check()
-                self.walk()
+                if(not self.fall):
+                    self.walk()
 
     def start_sequence(self):
         """At the beginning of the match, the robot walks forwards to move away from the edges."""
         self.gait_manager.command_to_motors(heading_angle=0)
 
     def walk(self):
-        normalized_x = self._get_normalized_opponent_x()
+        normalized_x = self._get_normalized_opponent_x() 
         desired_radius = (self.SMALLEST_TURNING_RADIUS / normalized_x) if abs(normalized_x) > 1e-3 else None
-        if self.heading_angle > 0 :
-            if self.counter > self.TIME_BEFORE_DIRECTION_CHANGE:
-                self.heading_angle = - self.heading_angle
-                self.counter = 0 
-        if self.heading_angle < 0 : 
-            if self.counter > self.TIME_BEFORE_DIRECTION_CHANGE: 
-                self.heading_angle = 0
-                self.counter = 0
+        if(normalized_x > 0): 
+            self.heading_angle = 3.14/4
+            self.counter = 0;  
+        elif(normalized_x < 0): 
+            self.heading_angle = -(3.14/4)
+            self.counter = 0 
+        elif(normalized_x == 0): 
+            return  
         self.counter += 1
         self.gait_manager.command_to_motors(desired_radius=desired_radius, heading_angle=self.heading_angle)
-        self.library.play('First')
+        self.library.play('Anglehandupdown')
 
     def _get_normalized_opponent_x(self):
         """Locate the opponent in the image and return its horizontal position in the range [-1, 1]."""
@@ -80,7 +101,5 @@ class Dark_Lord(Robot):
         return horizontal_coordinate * 2 / img.shape[1] - 1
 
 # create the Robot instance and run main loop
-wrestler = Dark_Lord()
+wrestler = Sultaan()
 wrestler.run()
-
-
