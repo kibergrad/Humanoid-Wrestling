6b8da832f9689bbaa37ed4ae3a1e4b67c323d5a7
kibergrad <105988797+kibergrad@users.noreply.github.com>
2023-05-16 13:01:56 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -13,9 +13,9 @@ from utils.finite_state_machine import FiniteStateMachine
 
 
 class Sultaan (Robot):
-    SMALLEST_TURNING_RADIUS = 0.1
+    SMALLEST_TURNING_RADIUS = 0.0 #0.1
     SAFE_ZONE = 0.75
-    TIME_BEFORE_DIRECTION_CHANGE = 0  # 8000 ms / 40 ms/
+    TIME_BEFORE_DIRECTION_CHANGE = 0  # 80
 
     def __init__(self):
         Robot.__init__(self)
