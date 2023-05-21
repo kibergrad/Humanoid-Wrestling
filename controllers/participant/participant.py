9c265e24d0288a524d7768e90f65abe84e3cb469
Siddharth Garg <siddharthg21@iitk.ac.in>
2023-05-21 22:55:59 +0530

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -3,8 +3,6 @@ from controller import Robot
 import sys
 sys.path.append('..')
 from utils.motion_library import MotionLibrary
-
-# Eve's locate_opponent() is implemented in this module:
 from utils.image_processing import ImageProcessing as IP
 from utils.fall_detection import FallDetection
 from utils.gait_manager import GaitManager
@@ -28,7 +26,7 @@ class Sultaan (Robot):
         self.gait_manager = GaitManager(self, self.time_step)
         self.heading_angle = 3.14 / 2
         self.counter = 0
-        self.library.add('Anglehandupdown', './New.motion', loop = True)
+        self.library.add('Anglehandupdown', './Shove.motion', loop = True)
         # self.library.add('Anglehandupdown', './.motion', loop = True)
         self.leds = {
             'rightf': self.getDevice('Face/Led/Right'), 
@@ -41,7 +39,6 @@ class Sultaan (Robot):
 
     def run(self):
         while self.step(self.time_step) != -1:
-            # We need to update the internal theta value of the gait manager at every step:
             t = self.getTime()
             self.leds['rightf'].set(0xff0000)
             self.leds['leftf'].set(0xff0000)
@@ -49,17 +46,13 @@ class Sultaan (Robot):
             self.leds['lefte'].set(0xff0000)
             self.leds['chest'].set(0xff0000)
             self.gait_manager.update_theta()
-            # if(self.fall_detector.detect_fall()): 
-            #     self.fall = True
             if 0.3 < t < 1:
                 self.start_sequence()
             elif t > 1:
                 self.fall_detector.check()
-                # if(not self.fall):
                 self.walk()
 
     def start_sequence(self):
-        """At the beginning of the match, the robot walks forwards to move away from the edges."""
         self.gait_manager.command_to_motors(heading_angle=0)
         
     def boundaryDetection(self):
@@ -70,39 +63,25 @@ class Sultaan (Robot):
     def walk(self):
         normalized_x = self._get_normalized_opponent_x() 
         desired_radius = (self.SMALLEST_TURNING_RADIUS / normalized_x) if abs(normalized_x) > 1e-3 else None
-        if(normalized_x): 
-            dist = self.boundaryDetection() 
-            print(dist)
-            if(dist <20):
-                self.heading_angle = 3.14/2
-                self.gait_manager.command_to_motors(desired_radius=desired_radius, heading_angle=self.heading_angle)
+        if(normalized_x != -1): 
+            # dist = self.boundaryDetection() 
+            # print(dist)
+            # if(dist <20):
+            #     self.heading_angle = 3.14/6
+            #     self.gait_manager.command_to_motors(desired_radius=desired_radius, heading_angle=self.heading_angle)
                 
-            else: 
-                self.heading_angle = 0
-                self.gait_manager.command_to_motors(desired_radius=0, heading_angle=self.heading_angle)
+            # else: 
+            self.heading_angle = 0
+            self.gait_manager.command_to_motors(desired_radius=0, heading_angle=self.heading_angle)
 
-        # elif(normalized_x != -1 and normalized_x > 0): 
-        #     self.heading_angle = 3.14/2
-        #     self.counter = 0;  
-        # elif(normalized_x < 0): 
-        #     self.heading_angle = -(3.14/2)
-        #     self.counter = 0 
-        # elif(normalized_x == 0): 
-        #     return  
         self.counter += 1
-        self.library.play('Anglehandupdown')
-
-
-            
 
     def _get_normalized_opponent_x(self):
-        """Locate the opponent in the image and return its horizontal position in the range [-1, 1]."""
         img = self.camera.get_image()
         _, _, horizontal_coordinate = IP.locate_opponent(img)
         if horizontal_coordinate is None:
             return 0
         return horizontal_coordinate * 2 / img.shape[1] - 1
 
-# create the Robot instance and run main loop
 wrestler = Sultaan()
 wrestler.run()
